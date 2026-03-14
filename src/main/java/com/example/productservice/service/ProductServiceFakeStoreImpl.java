f617af959dd2237b1335280ab88cf44cd41cee73
Aayush Singh <aayushs311@gmail.com>
2026-03-14 20:28:23 +0530

    fix

--- src/main/java/com/example/productservice/service/ProductServiceFakeStoreImpl.java
+++ src/main/java/com/example/productservice/service/ProductServiceFakeStoreImpl.java
@@ -40,18 +40,18 @@ public class ProductServiceFakeStoreImpl implements ProductService{
      */
     @Override
     public List<Product> getAllProducts() {
-        throw new RuntimeException();
-//        FakeStoreGetProductResponseDto[] response = restTemplate.getForObject(
-//                "https://fakestoreapi.com/products",
-//                FakeStoreGetProductResponseDto[].class
-//        );
-//        List<FakeStoreGetProductResponseDto> responseDtoList = Stream.of(response).toList();
-//
-//        List<Product> products = new ArrayList<>();
-//        for (FakeStoreGetProductResponseDto fakeStoreGetProductResponseDto: responseDtoList) {
-//            products.add(fakeStoreGetProductResponseDto.toProduct());
-//        }
-//       return products;
+//        throw new RuntimeException();
+        FakeStoreGetProductResponseDto[] response = restTemplate.getForObject(
+                "https://fakestoreapi.com/products",
+                FakeStoreGetProductResponseDto[].class
+        );
+        List<FakeStoreGetProductResponseDto> responseDtoList = Stream.of(response).toList();
+
+        List<Product> products = new ArrayList<>();
+        for (FakeStoreGetProductResponseDto fakeStoreGetProductResponseDto: responseDtoList) {
+            products.add(fakeStoreGetProductResponseDto.toProduct());
+        }
+       return products;
     }
 
 //    @Override
