12dfaa107a9b633cdb583afa61490cb368bab91a
Aayush Singh <aayushs311@gmail.com>
2026-04-10 01:53:19 +0530

    updated pom.xml

--- src/main/java/com/example/productservice/models/BaseModel.java
+++ src/main/java/com/example/productservice/models/BaseModel.java
@@ -22,7 +22,7 @@ import java.util.Date;
 @MappedSuperclass
 public class BaseModel {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private Date createdAt;
     private Date lastUpdatedAt;
